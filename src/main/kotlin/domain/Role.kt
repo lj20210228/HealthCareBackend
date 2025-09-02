10175584ef18e6773ceaf993148cb38b25c81fc5
lazar-jankovic <lazarj1007@gmail.com>
2025-09-02 15:15:30 +0200

    Dodata enum klasa Role za ulogu User klase

---
+++ src/main/kotlin/domain/Role.kt
@@ -0,0 +1,11 @@
+package com.example.domain
+
+
+/**
+ * Enum klasa Role za dodeljivanje uloge User klasi, moze biti lekar ili pacijent
+ * @author Lazar Janković
+ * @see User Sluzi za ovu klasu kako bi se useru dodelila uloga
+ */
+enum class Role {
+    ROLE_DOCTOR,ROLE_PATIENT
+}
