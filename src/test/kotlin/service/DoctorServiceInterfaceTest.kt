a62a30285df8dfc01903bc4810e36f9b51e982d2
lazar-jankovic <lazarj1007@gmail.com>
2025-09-05 15:15:06 +0200

    Dodati testovi za get funkcije

--- src/test/kotlin/service/DoctorServiceInterfaceTest.kt
+++ src/test/kotlin/service/DoctorServiceInterfaceTest.kt
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import java.io.File
 import kotlin.test.assertEquals
 
 abstract class DoctorServiceInterfaceTest {
@@ -14,6 +15,7 @@ abstract class DoctorServiceInterfaceTest {
     abstract fun getInstance(): DoctorServiceInterface
 
     var doctorService: DoctorServiceInterface?=null
+    val file=File("doctors.json")
     @BeforeEach
     fun setUp(){
         doctorService=getInstance()
@@ -21,6 +23,8 @@ abstract class DoctorServiceInterfaceTest {
     @AfterEach
     fun tearDown(){
         doctorService=null
+        file.writeText("")
+
     }
     @Test
      fun addDoctorTest_null(){
@@ -30,6 +34,7 @@ abstract class DoctorServiceInterfaceTest {
              }
          }
     }
+
     @Test
     fun addDoctorTest_vec_postoji(){
         val doctor1= Doctor("5","5","Pera Peric","Neurolog",20,0,"1",false)
@@ -50,6 +55,106 @@ abstract class DoctorServiceInterfaceTest {
         runBlocking {
             val added=doctorService?.addDoctor(doctor)
             assertEquals(added,doctorService?.getDoctorForId("1"))
+        }
+    }
+    @Test
+    fun getDoctorForIdTest_null(){
+        runBlocking {
+            assertThrows<NullPointerException> {
+                doctorService?.getDoctorForId(null)
+            }
+        }
+    }
+    @Test
+    fun getDoctorForIdTest_PrazanString(){
+        runBlocking {
+            assertThrows<NullPointerException> {
+                doctorService?.getDoctorForId("")
+            }
+        }
+    }
+    @Test
+    fun getDoctorForIdTest_ispravanId(){
+        runBlocking {
+            val doctor= Doctor("7","7","Pera Peric","Neurolog",20,0,"1",false)
+            doctorService?.addDoctor(doctor)
+            assertEquals(doctor,doctorService?.getDoctorForId("7"))
+
+        }
+    }
+    @Test
+    fun getAllDoctorsInHospitalTest_null(){
+        runBlocking {
+            assertThrows<NullPointerException> {
+                doctorService?.getAllDoctorsInHospital(null)
+            }
+        }
+    }
+    @Test
+    fun getAllDoctorInHospitalTest_PrazanString(){
+        runBlocking {
+            assertThrows<NullPointerException> {
+                doctorService?.getDoctorForId("")
+            }
+        }
+    }
+    @Test
+    fun getAllDoctorsInHospitalTest_ispravanId(){
+        runBlocking {
+            val doctor1= Doctor("7","7","Pera Peric","Neurolog",20,0,"1",false)
+            val doctor2= Doctor("1","1","Pera Mikic","Dermatolog",20,0,"1",false)
+            val doctor3= Doctor("2","2","Jovan Jovic","Lekar opste prakse",20,0,"1",false)
+
+
+            doctorService?.addDoctor(doctor1)
+            doctorService?.addDoctor(doctor2)
+            doctorService?.addDoctor(doctor3)
+            val doctorList=mutableListOf<Doctor>()
+            doctorList.add(doctor1)
+            doctorList.add(doctor2)
+            doctorList.add(doctor3)
+
+            assertEquals(doctorList,doctorService?.getAllDoctorsInHospital("1"))
+
+        }
+    }
+    @Test
+    fun getAllGeneralDoctorsInHospitalTest_null(){
+        runBlocking {
+            assertThrows<NullPointerException> {
+                doctorService?.getAllGeneralDoctorsInHospital(null)
+            }
+        }
+    }
+    @Test
+    fun getAllGeneralDoctorInHospitalTest_PrazanString(){
+        runBlocking {
+            assertThrows<NullPointerException> {
+                doctorService?.getAllGeneralDoctorsInHospital("")
+            }
+        }
+    }
+    @Test
+    fun getAllGeneralDoctorsInHospital_praznaLista(){
+        runBlocking {
+            assertEquals(emptyList(),doctorService?.getAllGeneralDoctorsInHospital("1"))
+        }
+    }
+
+    @Test
+    fun getAllGeneralDoctorsInHospitalTest_ispravanId(){
+        runBlocking {
+            val doctor1= Doctor("7","7","Pera Peric","Neurolog",20,0,"1",false)
+            val doctor2= Doctor("1","1","Pera Mikic","Dermatolog",20,0,"1",false)
+            val doctor3= Doctor("2","2","Jovan Jovic","Lekar opste prakse",20,0,"1",true)
+
+           doctorService?.addDoctor(doctor1)
+           doctorService?.addDoctor(doctor2)
+           doctorService?.addDoctor(doctor3)
+            val doctorList=listOf(doctor3)
+
+            assertEquals(doctorList,doctorService?.getAllGeneralDoctorsInHospital("1"))
+
         }
     }
 
