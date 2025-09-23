c841fcac19791e0c2ad85cb0782d0fb034665ba9
lazar-jankovic <lazarj1007@gmail.com>
2025-09-23 21:29:56 +0200

    Dodata klasa DayInWeek

---
+++ src/main/kotlin/domain/DayInWeek.kt
@@ -0,0 +1,10 @@
+package com.example.domain
+
+/**
+ * Spisak  dana, sluzice za [WorkTime]
+ * @author Lazar Janković
+ * @see WorkTime
+ */
+enum class DayInWeek {
+    MONDAY,TUESDAY,WEDNESDAY,THURSDAY,FRIDAY,SATURDAY,SUNDAY
+}
