780553e8c62af1ce8a6f22960e41d15c8e895226
lazar-jankovic <lazarj1007@gmail.com>
2025-10-03 19:52:39 +0200

    Dodat logicki sloj za upravljanje podacima o receptima

---
+++ src/main/kotlin/repository/recipe/RecipeRepositoryImplementation.kt
@@ -0,0 +1,92 @@
+package com.example.repository.recipe
+
+import com.example.domain.Recipe
+import com.example.response.BaseResponse
+import com.example.response.ListResponse
+import com.example.service.patient.PatientServiceInterface
+import com.example.service.recipe.RecipeServiceInterface
+
+/**
+ * Klasa koja implementira metode [RecipeRepository]
+ * @see RecipeRepository
+ * @see Recipe
+ * @see PatientServiceInterface
+ * @see RecipeServiceInterface
+ * @author Lazar Jankovic
+ */
+class RecipeRepositoryImplementation(val patientService: PatientServiceInterface,val serviceInterface: RecipeServiceInterface): RecipeRepository {
+    override suspend fun addRecipe(recipe: Recipe?): BaseResponse<Recipe> {
+        if (recipe==null)
+            return BaseResponse.ErrorResponse(message = "Ne mozete dodati recept bez podataka")
+        val added=serviceInterface.addRecipe(recipe)
+        if (added==null)
+            return BaseResponse.ErrorResponse(message = "Recept nije uspesno dodat")
+        return BaseResponse.SuccessResponse(data = added,"Recept uspesno dodat")
+    }
+
+    override suspend fun getAllRecipesForDoctor(doctorId: String?): ListResponse<Recipe> {
+        if (doctorId==null)
+            return ListResponse.ErrorResponse(message = "Niste prosledili ispravne podatke o lekaru")
+        val recipes=serviceInterface.getAllRecipesForDoctor(doctorId)
+        if (recipes.isEmpty())
+            return ListResponse.ErrorResponse(message = "Nije prepisan ni jedan recept")
+        return ListResponse.SuccessResponse(data = recipes, message = "Recepti uspesno pronadjeni")
+    }
+
+    override suspend fun getAllRecipesForPatient(patientId: String?): ListResponse<Recipe> {
+        if (patientId==null)
+            return ListResponse.ErrorResponse(message = "Niste prosledili ispravne podatke o pacijentu")
+        val recipes=serviceInterface.getAllRecipesForPatient(patientId)
+        if (recipes.isEmpty())
+            return ListResponse.ErrorResponse(message = "Nije prepisan ni jedan recept")
+        return ListResponse.SuccessResponse(data = recipes, message = "Recepti uspesno pronadjeni")
+    }
+
+    override suspend fun getRecipeForId(recipeId: String?): BaseResponse<Recipe> {
+        if (recipeId==null)
+            return BaseResponse.ErrorResponse(message = "Niste prosledili ispravne podatke o receptu")
+        val recipe=serviceInterface.getRecipeForId(recipeId)
+        if (recipe==null)
+            return BaseResponse.ErrorResponse(message = "Recept nije pronadjen")
+        return BaseResponse.SuccessResponse(data = recipe, message = "Recept uspesno pronadjen")
+    }
+
+    override suspend fun getRecipesForMedication(medication: String?): ListResponse<Recipe> {
+        if (medication==null)
+            return ListResponse.ErrorResponse(message = "Niste prosledili ispravne podatke o leku")
+        val recipes=serviceInterface.getRecipesForMedication(medication)
+        if (recipes.isEmpty())
+            return ListResponse.ErrorResponse(message = "Nema recepata sa ovim lekom")
+        return ListResponse.SuccessResponse(data = recipes, message = "Recepti uspesno pronadjeni")
+    }
+
+    override suspend fun getAllValidRecipesForDoctor(doctorId: String?): ListResponse<Recipe> {
+        if (doctorId==null)
+            return ListResponse.ErrorResponse(message = "Niste prosledili ispravne podatke o lekaru")
+        val recipes=serviceInterface.getAllValidRecipesForDoctor(doctorId)
+        if (recipes.isEmpty())
+            return ListResponse.ErrorResponse(message = "Nije prepisan ni jedan recept koji i dalje vazi")
+        return ListResponse.SuccessResponse(data = recipes, message = "Recepti uspesno pronadjeni")
+    }
+
+    override suspend fun getAllValidRecipesForPatient(patientId: String?): ListResponse<Recipe> {
+        if (patientId==null)
+            return ListResponse.ErrorResponse(message = "Niste prosledili ispravne podatke o pacijentu")
+        val recipes=serviceInterface.getAllValidRecipesForPatient(patientId)
+        if (recipes.isEmpty())
+            return ListResponse.ErrorResponse(message = "Nije prepisan ni jedan recept koji i dalje vazi")
+        return ListResponse.SuccessResponse(data = recipes, message = "Recepti uspesno pronadjeni")
+    }
+
+    override suspend fun getAllValidRecipesForPatientForJmbg(jmbg: String?): ListResponse<Recipe> {
+        if (jmbg==null)
+            return ListResponse.ErrorResponse(message = "Niste uneli ispravan jmbg")
+        val patient=patientService.getPatientByJmbg(jmbg)
+        if (patient==null)
+            return ListResponse.ErrorResponse(message = "Pacijent ne postoji")
+        val recipes=serviceInterface.getAllValidRecipesForPatient(patient.getId())
+        if (recipes.isEmpty())
+            return ListResponse.ErrorResponse(message = "Ne postoje vazeci recepti za ovog pacijenta")
+        return ListResponse.SuccessResponse(data = recipes, message = "Recepti uspesno pronadjeni")
+    }
+}
